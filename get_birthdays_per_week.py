dd85294f2f8dbd36a66293c002be7b308b676995
FTKV <mthkvv@gmail.com>
2023-06-22 17:22:21 +0100

    bug fix

--- get_birthdays_per_week.py
+++ get_birthdays_per_week.py
@@ -11,17 +11,19 @@ def get_birthdays_per_week(users):
     is_leap_year_flag = is_leap_year(next_monday_date.year)
     for user in users:
         date_of_birth = datetime.strptime(user["birthday"], DATE_FORMAT)
-        if not is_leap_year_flag:
-            if date_of_birth.month == 2 and date_of_birth.day == 29:
-                date_delta = datetime(year=next_monday_date.year, month=3, day=1).date() - next_monday_date
-            else:
-                date_delta = datetime(year=next_monday_date.year, month=date_of_birth.month, day=date_of_birth.day).date() - next_monday_date
-        if date_delta.days >= -2 and date_delta.days <=4:
-            if date_delta.days < 0:
-                key = 0
-            else:
-                key = date_delta.days
-            result[key].append(user["name"])
+        if not is_leap_year_flag and date_of_birth.month == 2 and date_of_birth.day == 29:
+            date_delta = datetime(year=next_monday_date.year, month=3, day=1).date() - next_monday_date
+        else:
+            date_delta = datetime(year=next_monday_date.year, month=date_of_birth.month, day=date_of_birth.day).date() - next_monday_date
+        delta_days = date_delta.days
+        if delta_days > 360:
+            delta_days -= 365 + is_leap_year_flag
+        elif delta_days < -360:
+            delta_days += 365 + is_leap_year_flag
+        if delta_days >= -2 and delta_days <=4:
+            if delta_days < 0:
+                delta_days = 0
+            result[delta_days].append(user["name"])
     
     for i in range(0, 5):
         if result[i]:
